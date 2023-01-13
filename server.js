93bf39e8a7e84616300902ed9da20e2597fe4a06
JesJessé Levandovski <jesse.levandovski@outlook.com>
2023-01-12 21:03:21 -0300

    identação de codigo

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { json } = require("stream/consumers");
 let data = require("./data.json");
 const app = express();
 
@@ -10,26 +9,31 @@ app.use(express.json());
 app.get("/clients", (req, res) => {
   return res.status(200).json(data);
 });
+
 app.get("/clients/:id", (req, res) => {
   const { id } = req.params;
+
   const client = data.find((cli) => cli.id == id);
 
   if (!client) return res.status(204).json();
 
   return res.json(client);
 });
+
 app.post("/clients", (req, res) => {
   const { name } = req.body;
+
   data.push({ id: data.length + 1, name: name });
-  return res
-    .status(200)
-    .json({
-      message: "Registration successfully added",
-      name: data[data.length],
-    });
+
+  return res.status(200).json({
+    message: "Registration successfully added",
+    name: data[data.length],
+  });
 });
+
 app.put("/clients/:id", (req, res) => {
   const { id } = req.params;
+
   const client = data.find((clie) => clie.id == id);
 
   if (!client) return res.status(204).json();
